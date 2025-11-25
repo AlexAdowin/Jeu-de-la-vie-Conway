52baa2c57365078bc17a580d0d1a62f17fe863a4
Alex Adowin <sedzanwingnannou@gmail.com>
2025-11-25 09:45:05 +0100

    modification de la grille

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from src.grille import Grille
 
-# Crée une petite grille pour tester
-grille = Grille(3, 4)
+grille = Grille(5, 5)
 
-# Affiche-la pour vérifier
+# Active quelques cellules pour faire un Blinker
+grille.definir_cellule(1, 2, 1)
+grille.definir_cellule(2, 2, 1)
+grille.definir_cellule(3, 2, 1)
+
+# Affiche pour vérifier
 for ligne in grille.cellules:
     print(ligne)
+'''
 
-print(f"Dimensions : {grille.lignes} x {grille.colonnes}")
-
-'''Résultat attendu :
-```
-[0, 0, 0, 0]
-[0, 0, 0, 0]
-[0, 0, 0, 0]
-Dimensions : 3 x 4'''
+**Résultat attendu :**
+'''
+[0, 0, 0, 0, 0]
+[0, 0, 1, 0, 0]
+[0, 0, 1, 0, 0]
+[0, 0, 1, 0, 0]
+[0, 0, 0, 0, 0]
--- src/grille.py
+++ src/grille.py
@@ -3,26 +3,44 @@ class Grille:
     
     def __init__(self , lignes ,colonnes) :
         
-        self.lignes = 50
+        self.lignes = lignes
         
-        self.colonnes = 50 
+        self.colonnes = colonnes
         
         self.cellules = [[0 for _ in range(self.colonnes)] for _ in range(self.lignes)] # Initialisation de la grille avec des cellules mortes (0)
         
         
-    def obtenir_cellule(self , lignes ,colonnes) :
+    def obtenir_cellule(self , ligne ,colonne) :
+        
+        if ligne < 0 or ligne >= self.lignes or colonne < 0 or colonne >= self.colonnes:
+            
+            raise 0
+     
         
         
-        
-        return self.cellules[lignes][colonnes]
+        return self.cellules[ligne][colonne]
         
         
     def definir_cellule(self , lignes ,colonnes ,etat) :
          
+         if lignes < 0 or lignes >= self.lignes or colonnes < 0 or colonnes >= self.colonnes:
+             
+             raise IndexError("Index de cellule hors limites")
+         
+         
+         if etat not in (0 ,1) :
+             
+             raise ValueError("L'état de la cellule doit être 0 (morte) ou 1 (vivante)")
          
          self.cellules[lignes][colonnes] = etat
          
     def obtenir_dimensions(self):
         
         
+        
         return self.lignes , self.colonnes
+    
+    
+    def reintialiser_grille(self):
+        
+        self.cellules = [[0 for _ in range(self.colonnes)] for _ in range(self.lignes)] # Réinitialisation de la grille avec des cellules mortes (0)
